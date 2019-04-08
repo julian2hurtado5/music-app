19c4f893b4f9a6b6332af5d6ad3a0945c7b685b0
Julian Hurtado <julian140514@outlook.com>
2019-04-08 18:07:56 -0500

    Creando vista form

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby '2.6.0'
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
 gem 'devise'
+gem "bootstrap_form", ">= 4.2.0"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
--- app/controllers/music_lists_controller.rb
+++ app/controllers/music_lists_controller.rb
@@ -2,4 +2,36 @@ class MusicListsController < ApplicationController
   def index
     @music_lists = MusicList.all
   end
+
+  def new
+    @music_list = MusicList.new
+    render :form
+  end
+
+  def show
+    @music_list = MusicList.find(params[:id])
+  end
+
+  def create
+    music_list = MusicList.new(music_list_parameters)
+    music_list.user_id = current_user.id
+    if music_list.save
+      redirect_to music_list_path(music_list)
+    else
+      p music_list.errors
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    @music_list = MusicList.find(params[:id])
+    @music_list.destroy
+
+    redirect_to :action => :index
+  end
+
+
+  def music_list_parameters
+    params.require(:music_list).permit(:nombre_cancion, :url)
+  end
 end
